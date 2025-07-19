546450638c0d3ae7317198dbde2b1dba559587a3
renaldyhidayatt <renaldyhidayatt@gmail.com>
2025-07-19 11:27:03 +0700

    Sekalian Build image || Add Loki, Promtail, AlertManager #2

--- crates/shared/src/utils/log.rs
+++ crates/shared/src/utils/log.rs
@@ -4,43 +4,44 @@ use tracing_appender::{
     non_blocking,
     rolling::{RollingFileAppender, Rotation},
 };
-use tracing_subscriber::{EnvFilter, fmt, prelude::*};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 pub fn init_logger(sdk_logger_provider: SdkLoggerProvider, component: &str) {
-    let log_file_name = format!("rust_app_{component}.log");
+    let log_dir = "/var/log/app";
+    let file_name = format!("rust_app_{component}.log");
+    let file_appender = RollingFileAppender::new(Rotation::DAILY, log_dir, file_name);
+    let (file_writer, guard) = non_blocking(file_appender);
 
-    let file_appender = RollingFileAppender::new(Rotation::DAILY, "/var/log/app", log_file_name);
-    let (non_blocking, _guard) = non_blocking(file_appender);
+    let file_layer = fmt::layer()
+        .with_writer(file_writer)
+        .with_ansi(false)
+        .json()
+        .with_filter(EnvFilter::new("info")); // Only info/warn/error
 
-    let filter_otel = EnvFilter::new("info")
+    let console_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("off")); // off by default (for prod)
+
+    let console_layer = fmt::layer()
+        .pretty()
+        .with_thread_names(true)
+        .with_ansi(true)
+        .with_filter(console_filter);
+
+    let otel_filter = EnvFilter::new("info")
         .add_directive("hyper=off".parse().unwrap())
         .add_directive("opentelemetry=off".parse().unwrap())
         .add_directive("tonic=off".parse().unwrap())
         .add_directive("h2=off".parse().unwrap())
         .add_directive("reqwest=off".parse().unwrap());
 
-    let otel_layer = OpenTelemetryTracingBridge::new(&sdk_logger_provider).with_filter(filter_otel);
-
-    let filter_fmt = EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-        EnvFilter::new("info").add_directive("opentelemetry=debug".parse().unwrap())
-    });
-
-    let console_layer = fmt::layer()
-        .with_thread_names(true)
-        .with_ansi(true)
-        .pretty()
-        .with_filter(filter_fmt);
-
-    let file_layer = fmt::layer()
-        .with_writer(non_blocking)
-        .with_ansi(false)
-        .json();
+    let otel_layer = OpenTelemetryTracingBridge::new(&sdk_logger_provider)
+        .with_filter(otel_filter);
 
     tracing_subscriber::registry()
-        .with(otel_layer)
-        .with(console_layer)
         .with(file_layer)
+        .with(console_layer)
+        .with(otel_layer)
         .init();
 
-    std::mem::forget(_guard); // or store it globally
+    std::mem::forget(guard);
 }
